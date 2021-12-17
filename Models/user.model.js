28e4f182f433574e57fe687979d23a28f007d676
mikk7021 <mikk7021@gmail.com>
2021-12-17 08:20:23 +0100

    English vars

--- Models/user.model.js
+++ Models/user.model.js
@@ -9,7 +9,7 @@ class UserModel {
     return new Promise((resolve, reject) => {
       const orderKey = req.query.orderBy || "u.id";
       const limit = req.query.limit ? `LIMIT ${req.query.limit}` : "";
-      let sql = `SELECT u.id, u.Bruger_id, u.Fornavn, u.Efternavn, u.Brugernavn, u.Password, u.Email, u.Fødselsdato, u.Køn
+      let sql = `SELECT u.id, u.User_id, u.Firstname, u.Lastname, u.Password, u.Email, u.date_of_birth, u.Gender
 						FROM user u 
 						ORDER BY ${orderKey} ${limit}`;
       db.query(sql, (err, result) => {
@@ -27,10 +27,10 @@ class UserModel {
       const orderKey = req.query.orderBy || "u.id";
 	  let keyword = req.query.keyword
       const limit = req.query.limit ? `LIMIT ${req.query.limit}` : "";
-      let sql = `SELECT u.id, u.Bruger_id, u.Fornavn, u.Efternavn, u.Brugernavn, u.Password, u.Email, u.Fødselsdato, u.Køn
+      let sql = `SELECT u.id, u.User_id, u.Firstname, u.Lastname, u.Password, u.Email, u.date_of_birth, u.Gender
 						FROM user u
-						WHERE Fornavn LIKE '%${keyword}%'
-						OR Efternavn LIKE '%${keyword}%'`
+						WHERE Firstname LIKE '%${keyword}%'
+						OR Lastname LIKE '%${keyword}%'`
 						;
       db.query(sql, (err, result) => {
         if (err) {
@@ -48,7 +48,7 @@ class UserModel {
 
   get = (req, res) => {
     return new Promise((resolve, reject) => {
-      const sql = `SELECT u.id, u.Bruger_id, u.Fornavn, u.Efternavn, u.Password, u.Email, u.Fødselsdato, u.Køn 
+      const sql = `SELECT u.id, u.User_id, u.Firstname, u.Lastname, u.Password, u.Email, u.date_of_birth, u.Gender
 							FROM user u  
 							WHERE u.id = ?`;
       db.query(sql, [req.params.id], (err, result) => {
@@ -64,7 +64,7 @@ class UserModel {
   create = async (req, res) => {
     return new Promise((resolve, reject) => {
       const arrFormValues = Object.values(req.body);
-      const sql = `INSERT INTO user(Bruger_id, Fornavn, Efternavn, Brugernavn, Password, Email, Fødselsdato, Køn) 
+      const sql = `INSERT INTO user(User_id, Firstname, Lastname, Username, Password, Email, date_of_birth, Gender) 
 							VALUES(?,?,?,?,?,?,?,?)`;
       db.query(sql, arrFormValues, (err, result) => {
         if (err) {
@@ -81,7 +81,7 @@ class UserModel {
       const arrFormValues = Object.values(req.body);
       console.log(arrFormValues);
       const sql = `UPDATE user
-							SET Brugernavn = ?, Password = ?, Email = ?
+							SET Username = ?, Password = ?, Email = ?
 							WHERE id = ?`;
       console.log(sql);
       db.query(sql, arrFormValues, (err, result) => {
@@ -98,7 +98,7 @@ class UserModel {
     return new Promise((resolve, reject) => {
       const arrFormValues = Object.values(req.body);
       console.log(arrFormValues);
-      const sql = `DELETE FROM user WHERE Fornavn = ?`;
+      const sql = `DELETE FROM user WHERE Firstname = ?`;
 
       db.query(sql, arrFormValues, (err, result) => {
         if (err) {
